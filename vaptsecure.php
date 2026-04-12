13ac79634aaf9d696ac2b4e400abf234b4805d7a
Tanveer Malik <tanveeratlogicx@gmail.com>
2026-04-12 14:30:58 +0500

    Bump version to 3.1.8 following SVP after merging Implementation-Dashboard into Master. Superadmin Interface Workbench and Client Dashboard layout fixes finalized.

--- vaptsecure.php
+++ vaptsecure.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: VAPTSecure Clean
  * Description: Ultimate VAPT and OWASP Security Plugin Builder.
- * Version: 3.1.6
+ * Version: 3.1.8
  * Author: Tanveer H. Malik
  * Author URI: https://vapt.copilot.com
  * License: GPL-2.0+
