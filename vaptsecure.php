76475b36646f111e37fb1be99456b5ebb06a030f
Tanveer Malik <tanveeratlogicx@gmail.com>
2026-05-06 11:17:54 +0500

    chore(vaptsecure-clean): release v3.7.3

--- vaptsecure.php
+++ vaptsecure.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: VAPTSecure Clean
  * Description: Ultimate VAPT and OWASP Security Plugin Builder.
- * Version: 3.7.2
+ * Version: 3.7.3
  * Author: Tanveer Hayat Malik
  * Author URI: https://vapt.copilot.com
  * License: GPL-2.0+
@@ -27,7 +27,7 @@ if (file_exists(dirname(__FILE__) . "/vendor/autoload.php")) {
 if (defined("VAPTSECURE_BUILD_VERSION")) {
     define("VAPTSECURE_VERSION", VAPTSECURE_BUILD_VERSION);
 } else {
-    define("VAPTSECURE_VERSION", "3.7.2"); // v3.7.2 - Patch release for build comment metadata formatting
+    define("VAPTSECURE_VERSION", "3.7.3"); // v3.7.3 - Patch release for runtime data packaging fix
 }
 if (!defined("VAPTSECURE_DATA_VERSION")) {
     define("VAPTSECURE_DATA_VERSION", "2.5.0");
